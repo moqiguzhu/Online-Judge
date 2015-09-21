463b218125527d9250853c94d3414916e0612e8c
moqiguzhu <moqiguzuh@163.com>
2015-09-21 09:30:26 +0800

    add annotations for median of two sorted arrays

--- OnlineJudge/src/Leetcode/MedianOfTwoSortedArrays.java
+++ OnlineJudge/src/Leetcode/MedianOfTwoSortedArrays.java
@@ -1,18 +1,47 @@
 package Leetcode;
 
 import java.util.Arrays;
+/**
+ * 
+ * @author moqiguzhu
+ * @version 1.0
+ * @date 2015-09-21
+ */
 
 public class MedianOfTwoSortedArrays {
+  /**
+   * 
+   * @param A 第一个数组
+   * @param B 第二个数组
+   * @return 两个数组的中位数
+   */
   public double findMedianSortedArrays(int A[], int B[]) {
     return help(A, A.length, B, B.length);
   }
 
+  /**
+   * 
+   * @param A 第一个排序好的数组
+   * @param m 第一个数组的长度
+   * @param B 第二个排序好的数组
+   * @param n 第二个数组的长度
+   * @return 两个数组的中位数
+   */
   public double help(int A[], int m, int B[], int n) {
     int l = (m + n + 1) >> 1;
     int r = (m + n + 2) >> 1;
     return (getkth(A, m, B, n, l) + getkth(A, m, B, n, r)) / 2.0;
   }
 
+  /**
+   * 
+   * @param s 较短数组
+   * @param m 较短数组的长度
+   * @param l 较长数组
+   * @param n 较长数组的长度
+   * @param k 第k大的数
+   * @return 两个数组第k大的数
+   */
   public int getkth(int s[], int m, int l[], int n, int k) {
     // let m <= n
     if (m > n)
@@ -22,7 +51,8 @@ public class MedianOfTwoSortedArrays {
     if (k == 1)
       return Math.min(s[0], l[0]);
 
-    int i = Math.min(m, k / 2), j = Math.min(n, k / 2);
+    // n > k
+    int i = Math.min(m, k / 2), j = k / 2;
     if (s[i - 1] > l[j - 1])
       return getkth(s, m, Arrays.copyOfRange(l, j, l.length), n - j, k - j);
     else
