af59a657a724edbe33dc253be59ff626697f61db
moqiguzhu <moqiguzhu@163.com>
2016-01-08 14:32:31 +0800

    add solution for power of two and contains duplicate problem

---
+++ OnlineJudge/src/math/PowerOfTwo.java
@@ -0,0 +1,16 @@
+package math;
+
+public class PowerOfTwo {
+  public boolean isPowerOfTwo(int n) {
+    return n > 0 && numberOfOneBits(n) == 1;
+  }
+  
+  public int numberOfOneBits(int n) {
+    int count = 0;
+    while(n != 0) {
+      count += (n & 1);
+      n >>>= 1;
+    }
+    return count;
+  }
+}
--- OnlineJudge/src/miscs/ContainsDuplicate.java
+++ OnlineJudge/src/miscs/ContainsDuplicate.java
@@ -1,18 +1,55 @@
 package miscs;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class ContainsDuplicate {
-  public boolean containsDuplicate(int[] nums) {
-    Set<Integer> set = new HashSet<>();
+  class N {
+    private int value;
+    private int index;
     
-    int curSize;
-    for(int x : nums) {
-      curSize = set.size();
-      set.add(x);
-      if(curSize == set.size())
-        return true;
+    public N(int value, int index) {
+      this.value = value;
+      this.index = index;
+    }
+  }
+  public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+    if(nums == null || nums.length < 2) {
+      return false;
+    }
+    
+    N[] Ns = new N[nums.length];
+    for(int i = 0; i < nums.length; i++) {
+      Ns[i] = new N(nums[i], i);
+    }
+    
+    Arrays.sort(Ns, new Comparator<N>() {
+
+      @Override
+      public int compare(N o1, N o2) {
+        // TODO Auto-generated method stub
+        if(o1.value > o2.value) {
+          return 1;
+        } else if(o1.value == o2.value) {
+          if(o1.index > o2.index) return 1;
+          else if(o1.index == o2.index) return 0;
+          else return -1;
+        } else {
+          return -1;
+        }
+      }
+      
+    });
+    
+    N cur;
+    for(int i = 0; i < Ns.length; i++) {
+      cur = Ns[i];
+      int j = i + 1;
+      while(j < Ns.length && (double)Ns[j].value - (double)cur.value <= t) {
+        if(Math.abs(Ns[j].index - cur.index) <= k)
+          return true;
+        j++;
+      }
     }
     
     return false;
