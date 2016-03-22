30bcc16739e746c28d124f5922cb322e5794dea5
moqiguzhu <moqiguzhu@users.noreply.github.com>
2016-03-22 23:23:53 +0800

    add implementation with bit operation

---
+++ OnlineJudge/src/bitoperation/BitAdd.java
@@ -0,0 +1,34 @@
+package hiho.coordinates;
+
+/**
+ * 
+ * @author moqiguzhu
+ * @date 2016-03-22
+ * @version 1.0
+ *
+ */
+public class BitAdd {
+  public int bitAdd(int a, int b) {
+    int res, carry;
+    do {
+      res = a ^ b;
+      carry = (a & b) << 1;     // << 的优先级比 & 高
+      a = res;
+      b = carry;
+    } while(carry != 0);
+    
+    return res;
+  }
+  
+  public int bitSubtract(int a, int b) {
+    // -b = ~b + 1
+    return bitAdd(a, bitAdd(~b, 1));
+  }
+  
+  public static void main(String[] args) {
+    BitAdd ba = new BitAdd();
+    System.out.println(ba.bitAdd(446, 123));
+    
+    System.out.println(ba.bitSubtract(123, 446));
+  }
+}
