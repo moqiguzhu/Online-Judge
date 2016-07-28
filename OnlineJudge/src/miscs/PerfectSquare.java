e7c0acc6f2e4285b16ff9fbac776123623ede471
moqiguzhu <moqiguzhu@users.noreply.github.com>
2016-07-28 17:45:54 +0800

    add solution for perfect square problem
    add solution for perfect square problem

---
+++ OnlineJudge/src/miscs/PerfectSquare.java
@@ -0,0 +1,29 @@
+package test;
+
+public class PerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        if(num < 0) {
+        	return false;
+        }
+        if(num <= 1) {
+        	return true;
+        }
+        
+        for(int i = 1; i < num / 2 + 1; i++) {
+        	if(i * i == num) {
+        		return true;
+        	}
+        	if(i * i > num) {
+        		return false;
+        	}
+        }
+        
+        return false;
+    }
+    
+    public static void main(String[] args) {
+    	PerfectSquare ps = new PerfectSquare();
+    	
+    	System.out.println(ps.isPerfectSquare(100));
+	}
+}
