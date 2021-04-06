1444ef8a19d3b4c533afa082f5b8085386b05724
andylei <andylei@tencent.com>
2021-04-06 15:57:20 +0800

    add logic

--- leetcode-cpp/test.cpp
+++ leetcode-cpp/test.cpp
@@ -1,3 +1,16 @@
 #include <vector>
+#include <iostream>
+#include <stdint.h>
 
-int main() { return 0; }
+int test1(int x, int y = 0) {
+    return x + y;
+}
+
+int main() {
+    std::cout << test1(1) << std::endl;
+    uint32_t x = 9999999999;
+    std::cout << x << std::endl;
+    std::cout << UINT32_MAX << std::endl;
+    return 0; 
+}
+
